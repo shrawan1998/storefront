a621d3a134ffa86c8e5656649e2f01384d66218a
shrawan-acuver <104749945+shrawan-acuver@users.noreply.github.com>
2022-06-12 01:16:37 +0530

    Creating migrations

--- storefront/settings.py
+++ storefront/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '761582'
     }
 }
 
