1b9ca8b39c88860f7171ba24ec41ea18140d0264
shrawan-acuver <104749945+shrawan-acuver@users.noreply.github.com>
2022-06-11 18:54:06 +0530

    Django debug toolbar

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# view -> request handler
+# request -> response
+def say_hello(request):
+    return render(request, 'hello.html', {'name': 'Django Project'})
