7f65677dff1c3c52036c62d26ca6b2c9973f26f6
shrawan-acuver <104749945+shrawan-acuver@users.noreply.github.com>
2022-06-16 21:39:00 +0530

    ORM query_set

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 # Create your views here.
 # view -> request handler
 # request -> response
 def say_hello(request):
+    query_set = Product.objects.all()
+    for product in query_set:
+        print(product)
     return render(request, 'hello.html', {'name': 'Django Project'})
