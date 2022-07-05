5fd5891e79a3a677bee1082ad5ef8d2114ea3672
shrawan-acuver <104749945+shrawan-acuver@users.noreply.github.com>
2022-07-05 15:48:17 +0530

    Managers and QuerySets

--- playground/views.py
+++ playground/views.py
@@ -7,6 +7,14 @@ from store.models import Product
 # request -> response
 def say_hello(request):
     query_set = Product.objects.all()
-    for product in query_set:
-        print(product)
+    # Product.objects gives us a manager (interface to the db)
+    # all method always return query set. It dosen't return object.
+    # To get object we need to iterte the query set
+    # for product in query_set:
+    #     print(product)
+    list(query_set)
+
+    count = Product.objects.count()
+    # Return actual object instead of query set
+    
     return render(request, 'hello.html', {'name': 'Django Project'})
