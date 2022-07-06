ae00edd730470033fef55127b74f113261626404
shrawan-acuver <104749945+shrawan-acuver@users.noreply.github.com>
2022-07-06 11:26:38 +0530

    Retrieving objects using filter() and managing excep using first()

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 # Create your views here.
 # view -> request handler
 # request -> response
 def say_hello(request):
-    try:
-        product = Product.objects.get(pk=0)
-        # pk stands for primary key
-    except ObjectDoesNotExist as e:
-        print(e)
+    product = Product.objects.filter(pk=0).first()
+    # If query set is empty then first method returns None
+    # pk stands for primary key
 
     query_set = Product.objects.all()
     # Product.objects gives us a manager (interface to the db)
