fa707b496383ed57b096fe84c81cc4e423a65e93
Shrawan Bagariya <skbagariya63@gmail.com>
2022-07-06 17:26:43 +0530

    Selecting table fields to query-initial

--- playground/views.py
+++ playground/views.py
@@ -6,10 +6,12 @@ from store.models import Product
 # view -> request handler
 # request -> response
 def say_hello(request):
-    # 0, 1, 2, 3, 4 -> limit: 5
-    query_set = Product.objects.all()[:5]
+    # Reading a related field which is collection in Product class
+    # It will create a query to inner join query
+    # values() return dict objects instead of Product objects
+    query_set = Product.objects.values('id', 'title', 'collection__title')
 
-    # 5, 6, 7, 8, 9 -> limit: 5, offset: 5 (for skipping first 5 elements)
-    query_set = Product.objects.all()[5:10]
+    # values_list() return tuple object
+    query_set = Product.objects.values_list('id', 'title', 'collection__title')
 
     return render(request, 'hello.html', {'name': 'Django Project', 'products': list(query_set)})
