40eaa5bf0402606a1d1f39ebb5e21d8fd1584986
shrawan-acuver <104749945+shrawan-acuver@users.noreply.github.com>
2022-07-06 14:44:38 +0530

    Sorting the data

--- playground/views.py
+++ playground/views.py
@@ -6,8 +6,12 @@ from store.models import Product
 # view -> request handler
 # request -> response
 def say_hello(request):
-    #Products: inventory = price
-    # F() -> constructor of F class
-    query_set = Product.objects.filter(inventory = F('unit_price'))
+    query_set = Product.objects.order_by('title') # asc
+    #query_set = Product.objects.order_by('-title') -> desc
+
+    query_set = Product.objects.order_by('unit_price', '-title')
+
+    product = Product.objects.order_by('unit_price')[0] # First product after sorting
+    product = Product.objects.earliest('unit_price') # First product
 
     return render(request, 'hello.html', {'name': 'Django Project', 'products': list(query_set)})
