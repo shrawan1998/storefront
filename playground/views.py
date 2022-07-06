1302db6fb5a521f499b4b3ebd4efb421e8497d09
shrawan-acuver <104749945+shrawan-acuver@users.noreply.github.com>
2022-07-06 13:36:21 +0530

    Referencing fields using F objects (compare)

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 # Create your views here.
 # view -> request handler
 # request -> response
 def say_hello(request):
-    # Products: inventory < 10 AND price < 20
-    query_set = Product.objects.filter(inventory__lt = 10, unit_price__lt = 20)
-
-    # chaining the filter method
-    query_set = Product.objects.filter(inventory__lt = 10).filter(unit_price__lt = 20)
-
-    # Products: inventory < 10 OR price < 20
-    query_set = Product.objects.filter(Q(inventory__lt = 10) | Q(unit_price__lt = 20))
-
-    #query_set = Product.objects.filter(Q(inventory__lt = 10) & ~Q(unit_price__lt = 20))
+    #Products: inventory = price
+    # F() -> constructor of F class
+    query_set = Product.objects.filter(inventory = F('unit_price'))
 
     return render(request, 'hello.html', {'name': 'Django Project', 'products': list(query_set)})
