fc95f549109935452172eac78f41e05dd1fff2a0
shrawan-acuver <104749945+shrawan-acuver@users.noreply.github.com>
2022-07-06 13:10:59 +0530

    Retrieving objects final commit

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
 from genericpath import exists
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
@@ -7,19 +8,28 @@ from store.models import Product
 # view -> request handler
 # request -> response
 def say_hello(request):
-    exists = Product.objects.filter(pk=0).exists()
-    # Return type of exists method is boolean
-    # pk stands for primary key
-
-    query_set = Product.objects.filter(unit_price__range = (20, 30))
-    # filtering the products which have unit price in range(20, 30)
-    #unit_price__gt = 20 --> Field lookups
-
-    #query_set = Product.objects.all()
+    query_set = Product.objects.all()
     # Product.objects gives us a manager (interface to the db)
     # all() always return query set. It dosen't return object.
     # To get object we need to iterte the query set
     #list(query_set)
+
+    product = Product.objects.get(id=1)
+
+    exists = Product.objects.filter(pk=0).exists()
+    # Return type of exists() is boolean
+    # pk stands for primary key
+
+    query_set = Product.objects.filter(unit_price__range = (20, 30))
+    # filtering the products which have unit price in range(20, 30)
+    # unit_price__gt = 20 --> Field lookups
+
+    query_set = Product.objects.filter(title__icontains = 'coffee')
+    # icontains -> case insensetive
+
+    #query_set = Product.objects.filter(description__isnull = True)
+
+    queryset = Product.objects.filter(collection__id__range = (1, 2, 3))
 
     count = Product.objects.count()
     # Return actual object instead of query set
