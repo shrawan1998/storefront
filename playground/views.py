8d99860f597af5bc8c578fb7b28f94fb1b0b8d55
Shrawan Bagariya <skbagariya63@gmail.com>
2022-07-06 16:53:34 +0530

    Limiting results using slice operator

--- playground/views.py
+++ playground/views.py
@@ -6,12 +6,10 @@ from store.models import Product
 # view -> request handler
 # request -> response
 def say_hello(request):
-    query_set = Product.objects.order_by('title') # asc
-    #query_set = Product.objects.order_by('-title') -> desc
+    # 0, 1, 2, 3, 4 -> limit: 5
+    query_set = Product.objects.all()[:5]
 
-    query_set = Product.objects.order_by('unit_price', '-title')
-
-    product = Product.objects.order_by('unit_price')[0] # First product after sorting
-    product = Product.objects.earliest('unit_price') # First product
+    # 5, 6, 7, 8, 9 -> limit: 5, offset: 5 (for skipping first 5 elements)
+    query_set = Product.objects.all()[5:10]
 
     return render(request, 'hello.html', {'name': 'Django Project', 'products': list(query_set)})
