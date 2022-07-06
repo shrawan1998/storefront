21f418745616929258213c10261823d6836b9cd9
shrawan-acuver <104749945+shrawan-acuver@users.noreply.github.com>
2022-07-06 13:26:41 +0530

    Complex lookups using Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,37 +1,20 @@
-from genericpath import exists
-from turtle import title
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Q
 from store.models import Product
 
 # Create your views here.
 # view -> request handler
 # request -> response
 def say_hello(request):
-    query_set = Product.objects.all()
-    # Product.objects gives us a manager (interface to the db)
-    # all() always return query set. It dosen't return object.
-    # To get object we need to iterte the query set
-    #list(query_set)
+    # Products: inventory < 10 AND price < 20
+    query_set = Product.objects.filter(inventory__lt = 10, unit_price__lt = 20)
 
-    product = Product.objects.get(id=1)
+    # chaining the filter method
+    query_set = Product.objects.filter(inventory__lt = 10).filter(unit_price__lt = 20)
 
-    exists = Product.objects.filter(pk=0).exists()
-    # Return type of exists() is boolean
-    # pk stands for primary key
+    # Products: inventory < 10 OR price < 20
+    query_set = Product.objects.filter(Q(inventory__lt = 10) | Q(unit_price__lt = 20))
 
-    query_set = Product.objects.filter(unit_price__range = (20, 30))
-    # filtering the products which have unit price in range(20, 30)
-    # unit_price__gt = 20 --> Field lookups
-
-    query_set = Product.objects.filter(title__icontains = 'coffee')
-    # icontains -> case insensetive
-
-    #query_set = Product.objects.filter(description__isnull = True)
-
-    queryset = Product.objects.filter(collection__id__range = (1, 2, 3))
-
-    count = Product.objects.count()
-    # Return actual object instead of query set
+    #query_set = Product.objects.filter(Q(inventory__lt = 10) & ~Q(unit_price__lt = 20))
 
     return render(request, 'hello.html', {'name': 'Django Project', 'products': list(query_set)})
